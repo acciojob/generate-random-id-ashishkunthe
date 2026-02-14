3be51ea4714c92c8eff1ba7013c8e531f511646a
Ashish Kunthe <160906505+ashishkunthe@users.noreply.github.com>
2026-02-15 00:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+	let str=""
+	let char_list="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklimnopqrstuvwxyz1234567890"
+	for(let i=0;i<l;i++){
+		str+=char_list.charAt(Math.floor(Math.random()*char_list.length))
+	}
+	return str
 }
 
 // Do not change the code below.
